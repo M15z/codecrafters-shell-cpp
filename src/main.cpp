e47cffef2ad45ec7399ea3a730632e7b64ee37d6
M15z <smaaz643@gmail.com>
2026-05-28 08:55:29 +0400

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,17 @@
 #include <string>
 
 using namespace std;
+bool isBuiltInCommand(string command) {
+    string arr[3] = {"exit", "echo", "type"};
+
+    for (int i = 0; i < 3; i++) {
+      if (command == arr[i])
+        return true;
+    };
+
+    return false;
+}
+
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
@@ -16,7 +27,13 @@ int main() {
       break;
     } else if (input.substr(0, 5) == "echo ") {
       std::cout << input.substr(5) << std::endl;
-    } else {
+    } else if (input.substr(0, 5) == "type ") {
+          if (isBuiltInCommand(input.substr(5)))
+              cout << input.substr(5) << " is a shell builtin" << endl;
+          else
+              std::cout << input.substr(5) << ": command not found" << std::endl;
+    }
+    else {
       std::cout << input << ": command not found" << std::endl;
     }
   }
