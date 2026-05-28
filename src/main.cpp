d07840f5e95ca5fd0862b8858c1bb56f584ef8c6
M15z <smaaz643@gmail.com>
2026-05-28 07:31:44 +0400

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
@@ -8,4 +9,7 @@ int main() {
 
   // TODO: Uncomment the code below to pass the first stage
   std::cout << "$ ";
+  std::string prompt;
+  std::cin >> prompt;
+  std::cout << prompt << ": command not found";
 }
