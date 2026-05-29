63d7d3f8fb519bea8c9bcc9fbcf8cab7eb70eadb
M15z <smaaz643@gmail.com>
2026-05-29 13:50:55 +0400

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,37 @@
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <ranges>
+#include <string>
+#include <unistd.h>
 
 using namespace std;
 bool isBuiltInCommand(string command) {
     string arr[3] = {"exit", "echo", "type"};
 
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < arr->length(); i++) {
       if (command == arr[i])
         return true;
     };
 
     return false;
+}
+
+bool printPathOfCommand(string command) {
+  string pathEnv = getenv("PATH");
+  stringstream ssPath(pathEnv);
+
+  string path;
+  while (getline(ssPath, path, ':')) {
+    string fullPath = path + '/' + command;
+
+    if (access(fullPath.c_str(), X_OK) == 0) {
+      cout << command << " is " << fullPath << endl;
+      return true;
+    }
+  }
+
+  return false;
 }
 
 int main() {
@@ -30,9 +51,13 @@ int main() {
     } else if (input.substr(0, 5) == "type ") {
           if (isBuiltInCommand(input.substr(5)))
               cout << input.substr(5) << " is a shell builtin" << endl;
-          else
+          else if(!isBuiltInCommand(input.substr(5))) {
+            if (!printPathOfCommand(input.substr(5)))
               std::cout << input.substr(5) << ": not found" << std::endl;
-    }
+
+          }
+
+        }
     else {
       std::cout << input << ": command not found" << std::endl;
     }
